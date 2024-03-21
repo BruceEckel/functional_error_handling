cac55f65907b6940a0aeb4780b04ff2aa1a5f76a
Bruce Eckel <MindviewInc@gmail.com>
2024-03-21 12:30:43 -0600

    added output strings

--- src/functional_error_handling/comprehension1.py
+++ src/functional_error_handling/comprehension1.py
@@ -6,3 +6,12 @@ def f1(i: int) -> int:
 
 
 print([f1(i) for i in range(5)])
+"""
+Traceback (most recent call last):
+  File "C:\git\functional_error_handling\src\functional_error_handling\comprehension1.py", line 8, in <module>
+    print([f1(i) for i in range(5)])
+           ^^^^^
+  File "C:\git\functional_error_handling\src\functional_error_handling\comprehension1.py", line 3, in f1
+    raise ValueError("i cannot be 3")
+ValueError: i cannot be 3
+"""
--- src/functional_error_handling/comprehension2.py
+++ src/functional_error_handling/comprehension2.py
@@ -10,6 +10,9 @@ def f2(i: int) -> Result[int, str]:
 
 results = [f2(i) for i in range(5)]
 print(results)
+"""
+[Ok(value=0), Ok(value=2), Ok(value=4), Err(error='i cannot be 3'), Ok(value=8)]
+"""
 
 for result in results:
     match result:
@@ -17,11 +20,23 @@ for result in results:
             print(value)
         case Err(error):
             print(f"Error: {error}")
+"""
+0
+2
+4
+Error: i cannot be 3
+8
+"""
 
 
+# Composition: return type enforced
 def g(i: int) -> Result[int, str]:
     return f2(i)
 
 
 print(g(1))
 print(g(5))
+"""
+Ok(value=2)
+Ok(value=10)
+"""
