914605d7aca5a7acc0736a73163db5e4e745d9b5
Bruce Eckel <mindviewinc@gmail.com>
2024-04-03 21:28:00 -0600

    Update composed.py

--- src/functional_error_handling/composed.py
+++ src/functional_error_handling/composed.py
@@ -7,42 +7,33 @@ from typing import Callable
 # Is there a way to catch an exception and convert it to a typed Failure?
 
 
-def f4(arg: int) -> Result[int, str]:
+def a(arg: int) -> Result[int, str]:
     if arg == 1:
-        return Failure(f"f4({arg = })")
+        return Failure(f"a({arg = })")
     return Success(arg)
 
 
 # Convert an existing function:
 @attempt
-def f5(arg: int) -> int:  # becomes: Result[int, int]
-    print(f"f5: {arg = }, {1 / arg = }")
+def b(arg: int) -> int:  # becomes Result[int, int]
+    print(f"b({arg}): {1 / arg}")
     return arg
 
 
-# This might be redundant
-def f6(arg: int) -> Result[str, str]:
-    if arg == 2:
-        return Failure(f"f6({arg = })")
-    return Success(str(arg))
-
-
 # Use an exception (but don't raise it):
-def f7(arg: str) -> Result[str, ValueError]:
-    if arg == "-1":
-        return Failure(ValueError(f"f7({arg = })"))
-    return Success(arg + "!")
+def c(arg: int) -> Result[str, ValueError]:
+    if arg == -1:
+        return Failure(ValueError(f"c({arg = })"))
+    return Success(f"{arg}!")
 
 
-# Is Result type correct here?
-composed: Callable[[int], Result[str, ValueError | str]] = pipe(
-    f4,
-    bind(f5),
-    bind(f6),
-    bind(f7),
+composed: Callable[[int], Result[str, ValueError | str | int]] = pipe(
+    a,
+    bind(b),
+    bind(c),
 )
 
-inputs = range(-1, 4)
+inputs = range(-1, 3)
 print(f"inputs = {list(inputs)}")
 outputs = [composed(i) for i in inputs]
 
@@ -50,7 +41,9 @@ for e in zip(inputs, outputs):
     print(f"{e[0]:>2}: {e[1]}")
 
 # Extract results, converting failure to None:
-print([r.value_or(None) for r in outputs])
+outputs2 = [r.value_or(None) for r in outputs]
+print(outputs2)
+print(list(filter(None, outputs2)))
 
 # Another way to extract results:
 for r in outputs:
