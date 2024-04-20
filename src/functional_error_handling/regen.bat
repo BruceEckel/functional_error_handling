f7231c87e7a03e9aa95720b23c9854e230d61d2c
Bruce Eckel <MindviewInc@gmail.com>
2024-04-20 12:29:29 -0600

    Update regen.bat

--- src/functional_error_handling/regen.bat
+++ src/functional_error_handling/regen.bat
@@ -1,7 +1,7 @@
 echo OFF
+del *_modified.py
+del *_temp.py
 cls
-del *modified.py
-del *temp.py
 echo --- Remove all 'console ==' outputs, regenerate them, and test everything ---
 python .\update_output.py * --clear
 : rye test
